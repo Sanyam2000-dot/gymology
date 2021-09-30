4dbc87ef8a43c8853ef3ae722f6b38a4efe4fb8b
Sanyam Mehendiratta <sanyammehendiratta2000@gmail.com>
2021-09-30 20:47:35 +0530

    files added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
+import GymData from "./GymData";
 
 function App() {
+  const [item, setItem] = useState("Exercises");
+  const select = (item) => {
+    setItem(item);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="App">
+        <h1 style={{ fontSize: "2.5rem" }}>Gymology</h1>
+        {Object.keys(GymData).map((data) => (
+          <button className="buttons" onClick={() => select(data)}>
+            {data}
+          </button>
+        ))}
+      </div>
+
+      <div
+        style={{
+          width: "50%",
+
+          margin: "auto",
+        }}
+      >
+        <div>
+          <ul
+            style={{
+              paddingInlineStart: "0",
+              textAlign: "center",
+              margin: "auto",
+            }}
+          >
+            {GymData[item].map((tools) => (
+              <li
+                key={tools.name}
+                style={{
+                  listStyle: "none",
+                  padding: "1rem",
+                  border: "1px solid black",
+                  width: "90%",
+                  marginBottom: "1rem",
+                  fontSize: "1.5rem",
+                  backgroundColor: "#fff5ee",
+
+                  borderRadius: "0.5rem",
+                }}
+              >
+                {tools.name}{" "}
+                <div style={{ fontWeight: "lighter", fontStyle: "italic" }}>
+                  {" "}
+                  {tools.description}{" "}
+                </div>
+                <div style={{ fontWeight: "lighter" }}> {tools.rating} </div>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    </>
   );
 }
 
---
+++ src/GymData.js
@@ -0,0 +1,47 @@
+const GymData = {
+    Exercises: [{
+            name: "Lunges",
+            description: "Challenging your balance is an essential part of a well-rounded exercise routine. Lunges do just that, promoting functional movement, while also increasing strength in your legs and glutes.",
+        },
+        {
+            name: " Pushups",
+            description: " Pushups are one of the most basic, yet effective, body weight moves you can perform because of the number of muscles that are recruited to perform them.",
+        },
+        {
+            name: "Squats",
+            description: "Squats increase lower body and core strength, as well as flexibility in your lower back and hips. Because they engage some of the largest muscles in the body, they also pack a major punch in terms of calories burned",
+        },
+        {
+            name: "Dumbbell rows",
+            description: "Not only will these make your back look killer in that dress, but dumbbell rows are also another compound exercise that strengthens multiple muscles in your upper body. Choose a moderate-weight dumbbell and ensure that you’re squeezing at the top of the movement.",
+        },
+    ],
+    Meals: [{
+            name: "Carb",
+            description: "A low-carb diet is a diet that restricts carbohydrates, such as those found in sugary foods, pasta and bread. It is high in protein, fat and healthy vegetables.",
+        },
+        {
+            name: "Keto",
+            description: "The ketogenic diet, or keto for short, is a very low-carb diet that’s high in fat and moderate in protein. It’s similar to other grain-free and low-carb diets, like paleo and Atkins, and calls for eating meats, dairy, eggs, fish, nuts, butters, oils, and non-starchy vegetables.",
+        },
+        {
+            name: "Protein",
+            description: "High-protein diets typically include large quantities of protein and only a small amount of carbohydrate. Most people can follow a high-protein diet by eating meat, fish, dairy products, beans and legumes, eggs, and vegetables that are relatively rich in protein, such as asparagus and spinach",
+        },
+    ],
+    Supplements: [{
+            name: "Whey Protein",
+            description: "Much of the protein found in protein bars, drinks and powders is derived from milk. When milk is processed to form cheese or yogurt, the remaining liquid is called whey ",
+        },
+        {
+            name: "BCAA",
+            description: "BCAA supplements are commonly taken to boost muscle growth and enhance exercise performance. They may also help with weight loss and reducing fatigue after exercise. ",
+        },
+        {
+            name: "Creatine",
+            description: "Creatine is an organic compound that, when taken as a supplement, can help increase muscle mass, strength, and exercise performance.",
+        },
+    ],
+};
+
+export default GymData;
